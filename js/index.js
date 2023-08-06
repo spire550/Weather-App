1a359f2b859caf3a7188daa889dbea60006044bb
spire550 <43911342+spire550@users.noreply.github.com>
2023-08-06 11:37:39 +0300

    Add Geolocation Api

--- js/index.js
+++ js/index.js
@@ -41,8 +41,57 @@ let days = [
   "Friday",
   "Saturday",
 ];
+function getCoordintes() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
 
-async function getWeatherApi(city = "Kafr Ash Shaykh") {
+	function success(pos) {
+		var crd = pos.coords;
+		var lat = crd.latitude.toString();
+		var lng = crd.longitude.toString();
+		var coordinates = [lat, lng];
+		getCity(coordinates);
+		return;
+
+	}
+
+	function error(err) {
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function getCity(coordinates) {
+	var xhr = new XMLHttpRequest();
+	var lat = coordinates[0];
+	var lng = coordinates[1];
+
+	xhr.open('GET', `
+https://us1.locationiq.com/v1/reverse.php?key=pk.3af21b5c11eaa2162cb13b75ff60d08c&lat=` +
+	lat + "&lon=" + lng + "&format=json", true);
+	xhr.send();
+	xhr.onreadystatechange = processRequest;
+	xhr.addEventListener("readystatechange", processRequest, false);
+
+	function processRequest(e) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var response = JSON.parse(xhr.responseText);
+			var city = response.address.city;
+			let mychoice=city;
+      getWeatherApi(mychoice)
+			return;
+		}
+	}
+}
+
+getCoordintes();
+
+
+async function getWeatherApi(city) {
   apiResponse = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=d3de10c756d44292b9b210129230308&q=${city}&days=3&aqi=no&alerts=no`
   );
